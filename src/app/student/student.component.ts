a45f816b2ee97278d9ef54bed85697fab1d640bd
kt-4 <kashyap.qtonz@yahoo.com>
2022-06-23 18:58:25 +0530

    change add markdata

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,5 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StudentService } from '../services/student.service';
 
@@ -13,21 +14,36 @@ import { StudentService } from '../services/student.service';
 export class StudentComponent implements OnInit {
   students:any = []
   Onestudent:any
-  Deletestudent:any
-
+  Addstudent:any
+  id:any
 
   constructor(public studentService:StudentService,private route:ActivatedRoute,private router:Router) { }
  
   ngOnInit(): void {
   this.getStudnets()
+  this.id = this.route.snapshot.params['id'];
+  this.studentService.getStudentById(this.id).subscribe((data:any)=>{
+    this.students = data;
+  })
+  
    
   }
 
   form= new FormGroup({
     name:new FormControl('',Validators.required),
     roll:new FormControl('',Validators.required),
-    // subject:new FormControl('',Validators.required),
-    // marks:new FormControl('',Validators.required)
+      //subject:new FormControl('',Validators.required)
+    
+  })
+  subform = new FormGroup({
+    subject:new FormControl([],Validators.required),
+    marks:new FormControl([],Validators.required)
+  })
+
+  editForm = new FormGroup({
+    name:new FormControl('',Validators.required),
+    roll:new FormControl('',Validators.required),
+
   })
 
   getStudnets(){
@@ -64,5 +80,36 @@ export class StudentComponent implements OnInit {
       })
     }
 
+
+    //  addData(){
+    //   // console.log(this.form.value)
+    //   // this.studentService.createNewStudent(this.form.value).subscribe((markdata)=>{
+    //   //   console.log(markdata)
+    //   //   alert('studnet added')
+    //   // })
+    // }
+     
+    addData(markid:any){
+       this.studentService.getStudentById(markid).subscribe((data:any)=>{
+        this.Addstudent = data
+     })
+     
+     
+      // this.studentService.createNewStudent(this.subform.value).subscribe((subjectData)=>{
+      // console.log(subjectData)
+      // alert('subject added')
+      // })
+   }
+
+   subStudent(){
+    console.log(this.subform.value)
+   }
+
+   edit(){
+     console.log(this.editForm.value)
+     this.studentService.updateStudentById(this.id,this.editForm.value).subscribe(updateres =>{
+       console.log(updateres)
+     })
+   }
    
 }
